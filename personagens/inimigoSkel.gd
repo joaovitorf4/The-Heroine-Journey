f07281f590236ac532edff519700a60a1b817ae3
João Vitor Lopes Fagundes <70524677+joaovitorfod4@users.noreply.github.com>
2022-12-04 22:29:19 -0300

    Add files via upload

--- personagens/inimigoSkel.gd
+++ personagens/inimigoSkel.gd
@@ -3,29 +3,35 @@ extends KinematicBody2D
 var flip = true
 var posicao_inicial
 var posicao_final
-var velocidade = 0.5
+var velocidade = 0.9
 export var health = 1
 var hitted = false
 
 func _ready():
 	$AnimatedSprite.play("Walk")
 	posicao_inicial = $".".position.x
-	posicao_final = posicao_inicial + 100
+	posicao_final = posicao_inicial + 500
+	
 	
 func _process(delta):
 #	print($".".position.x)
 	if(posicao_inicial <= posicao_final and flip):
-		$".".position.x += velocidade
-		$AnimatedSprite.flip_h = true
+		$".".position.x -= velocidade
+		$AnimatedSprite.flip_h = false
 		if($".".position.x >= posicao_final):
 			flip = false
 
 	if($".".position.x >= posicao_inicial and !flip):
-		$".".position.x -= velocidade
-		$AnimatedSprite.flip_h = false
+		$".".position.x += velocidade
+		$AnimatedSprite.flip_h = true
 		if($".".position.x <= posicao_inicial):
 			flip = true
-func _on_hitbox_body_entered(body: Node) -> void:
+
+func _on_AnimatedSprite_animation_finished():
+	pass # Replace with function body.
+
+
+func _on_hitbox_body_entered(body):
 	hitted = true
 	body.velocity.y -= 250
 	health -= 1
--- personagens/inimigoWorm.gd
+++ personagens/inimigoWorm.gd
@@ -10,27 +10,32 @@ var hitted = false
 func _ready():
 	$AnimatedSprite.play("Walk")
 	posicao_inicial = $".".position.x
-	posicao_final = posicao_inicial + 100
+	posicao_final = posicao_inicial + 390
+	
 	
 func _process(delta):
-#	print($".".position.x)
+	if $ray_wall.is_colliding():
+		$AnimatedSprite.flip_h != $AnimatedSprite.flip_h
 	if(posicao_inicial <= posicao_final and flip):
 		$".".position.x += velocidade
-		$AnimatedSprite.flip_h = true
+		$AnimatedSprite.flip_h = false
 		if($".".position.x >= posicao_final):
 			flip = false
 
 	if($".".position.x >= posicao_inicial and !flip):
 		$".".position.x -= velocidade
-		$AnimatedSprite.flip_h = false
+		$AnimatedSprite.flip_h = true
 		if($".".position.x <= posicao_inicial):
 			flip = true
+
+
 func _on_hitbox_body_entered(body: Node) -> void:
 	hitted = true
 	body.velocity.y -= 250
 	health -= 1
 	if health < 1:
 		$AnimatedSprite.play("Death")
-		yield(get_tree().create_timer(1), "timeout")
+		velocidade = 0
+		yield(get_tree().create_timer(1.5), "timeout")
 		queue_free()
-		get_node("ray_wall/hitbox/collision").set_deferred("disable", true)
+	
